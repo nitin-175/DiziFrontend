4ca09e803820efc7369d040e7dce976f6c1ae317
Aryan Singh <er.aryansingh09@gmail.com>
2025-08-14 15:50:59 +0530

    feature: add Hero2 section

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@
 import './App.css'
 import React from "react";
 import { Carousel } from "flowbite-react";
+import Navbar from './components/Navbar';
+import Hero2 from './components/Hero2';
 import Footer from './components/Footer';
-
-
-
-
 
 
 
@@ -14,6 +12,8 @@ function App() {
 
   return (
     <div>
+      <Navbar />
+      <Hero2 />
       <Footer/>
       
     </div>
